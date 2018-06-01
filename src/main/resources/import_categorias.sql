24f73f6fa61cab074aba80a31811acf7ddbd7c71
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-06-01 03:29:54 -0500

    import_categorias.sql added

---
+++ src/main/resources/import_categorias.sql
@@ -0,0 +1,26 @@
+
+/*CATEGORIAS*/
+INSERT INTO categoria(nombre)
+VALUES('Vivienda');
+INSERT INTO categoria(nombre)
+VALUES('Alimentos');
+INSERT INTO categoria(nombre)
+VALUES('Transporte');
+INSERT INTO categoria(nombre)
+VALUES('Seguro');
+INSERT INTO categoria(nombre)
+VALUES('Entretenimiento');
+INSERT INTO categoria(nombre)
+VALUES('Niños');
+INSERT INTO categoria(nombre)
+VALUES('Prestamos');
+INSERT INTO categoria(nombre)
+VALUES('Impuestos');
+INSERT INTO categoria(nombre)
+VALUES('Cuidados Personales');
+INSERT INTO categoria(nombre)
+VALUES('Mascotas');
+INSERT INTO categoria(nombre)
+VALUES('Regalos y Donaciones');
+INSERT INTO categoria(nombre)
+VALUES('Asesoria Juridica');
