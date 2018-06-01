9351f4a8c793ac12ba67bcbad6f2244a0cb421ec
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-06-01 01:54:15 -0500

    DetalleGastoController added - views fixed

---
+++ src/main/java/pe/edu/upc/controllers/DetalleGastoController.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/detallegasto")
+public class DetalleGastoController {
+
+}
